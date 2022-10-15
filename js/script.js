9e5ecac92822c8171b0888c425a02dfd813b7b8d
JA Gonzalez <jatgonzalez33@gmail.com>
2022-10-14 20:56:56 -0500

    Add: Display de todos los productos

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 // console.log("link");
 
-fetch("http://localhost:8080/api/products")
+fetch("../json/data.json")
     .then(response => response.json())
     .then(array => {
         array.forEach(element => {
+            let cardCounter;
             // console.log(element);
             /***
              * Producto nombre
              */
             let productName = element.productName;
+            productName = productName.toLowerCase();
+            productName = productName.charAt(0).toUpperCase() + productName.slice(1);
 
             /***
              * Precio
@@ -20,5 +23,20 @@ fetch("http://localhost:8080/api/products")
              * Imagen
              */
             let img = element.urlImage;
+            if(img == null || img == "") img="../img/templatebottle.png" //img = "https://images.pexels.com/photos/1727829/pexels-photo-1727829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+
+            let divProductos = document.getElementById("productos");
+
+            let node = `<div class="card col-4">
+          <img src="${img}" class="card-img-top" alt="...">
+          <div class="card-body">
+          <p class="card-text">${productName}</p>
+          <h5 class="card-title">Precio: $ ${price}</h5>
+          </div>
+        </div>`;
+
+        divProductos.insertAdjacentHTML("afterbegin", node);
+
         })
     });
