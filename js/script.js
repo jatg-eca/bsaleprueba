2a1362026d3c82162516e612081f29bb909ac8cd
JA Gonzalez <jatgonzalez33@gmail.com>
2022-10-14 19:39:12 -0500

    Add: Consumiendo api rest pruebas

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // console.log("link");
 
 fetch("http://localhost:8080/api/products")
-    .then(data => (data.json))
-    
+    .then(response => response.json())
+    .then(array => {
+        array.forEach(element => {
+            // console.log(element);
+            /***
+             * Producto nombre
+             */
+            let productName = element.productName;
+
+            /***
+             * Precio
+             */
+            let price = element.price;
+            if(element.discount != 0) price = (element.price - (element.price * (element.discount/100)));
+            
+            /**
+             * Imagen
+             */
+            let img = element.urlImage;
+        })
+    });
